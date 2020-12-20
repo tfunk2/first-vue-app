4eac327607553ee8ab4af3fd732c2f64a1547ed5
tfunk2 <tyler.j.funk2@gmail.com>
2020-12-20 03:02:21 -0700

    added button with event to randomize a variable that displays hobbies

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ const app = Vue.createApp({
       age: 28,
       hometown: "Kansas City, MO",
       hobbies: ["Golf", "Coding", "Video Games"],
+      displayedHobby: `Click the button to see this person's hobbies!`,
       avatarImage: "https://www.w3schools.com/howto/img_avatar.png",
     };
   },
   methods: {
-    getRandomHobby() {
-      const randomIndex = Math.floor(Math.random() * this.hobbies.length);
-      return this.hobbies[randomIndex];
+    setDisplayedHobby() {
+      let randomIndex = Math.floor(Math.random() * this.hobbies.length);
+
+      if(this.displayedHobby === this.hobbies[randomIndex]) {
+        if(randomIndex === 0) {
+          randomIndex = 1;
+        } else {
+          randomIndex -= 1
+        }
+      }
+      
+      this.displayedHobby = this.hobbies[randomIndex];
     },
   },
 });
